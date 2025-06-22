7f3ee4c5b1611722e96c6a91500dd704b355cb5a
김동현 <47862506+PH5555@users.noreply.github.com>
2025-06-22 09:27:58 +0900

    Style: 데모 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [zktransfer, setZktransfer] = useState({
+    senderEoa: '',
+    senderEoaSk: '',
+    senderEnaPk: '',
+    senderEnaSk: '',
+    reveiverEoa: '',
+    amount: '',
+  });
+
+  const [zkDeposit, setZkDeposit] = useState({
+    eoa: '',
+    eoaSk: '',
+    enaPk: '',
+    enaSk: '',
+    amount: '',
+  });
+
+  const onChangeZkTransfer = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setZktransfer(prev => {
+      return {...prev, [name] : value}
+    });
+  };
+
+  const onChangeZkDeposit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setZkDeposit(prev => {
+      return {...prev, [name] : value}
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>private 전송</h2>
+        <h5>sender eoa</h5>
+        <input name='senderEoa' value={zktransfer.senderEoa} onChange={onChangeZkTransfer}/>
+        <h5>sender eoa sk</h5>
+        <input name='senderEoaSk' value={zktransfer.senderEoaSk} onChange={onChangeZkTransfer}/>
+        <h5>sender ena pk</h5>
+        <input name='senderEnaPk' value={zktransfer.senderEnaPk} onChange={onChangeZkTransfer}/>
+        <h5>sender ena sk</h5>
+        <input name='senderEnaSk' value={zktransfer.senderEnaSk} onChange={onChangeZkTransfer}/>
+        <h5>receiver eoa</h5>
+        <input name='reveiverEoa' value={zktransfer.reveiverEoa} onChange={onChangeZkTransfer}/>
+        <h5>amount</h5>
+        <input name='amount' value={zktransfer.amount} onChange={onChangeZkTransfer}/>
+        <br/>
+        <button>전송</button>
+      </div>
+
+      <div>
+        <h2>public to private 전환</h2>
+        <h5>eoa</h5>
+        <input name='eoa' value={zkDeposit.eoa} onChange={onChangeZkDeposit}/>
+        <h5>eoa sk</h5>
+        <input name='eoaSk' value={zkDeposit.eoaSk} onChange={onChangeZkDeposit}/>
+        <h5>ena pk</h5>
+        <input name='enaPk' value={zkDeposit.enaPk} onChange={onChangeZkDeposit}/>
+        <h5>ena sk</h5>
+        <input name='enaSk' value={zkDeposit.enaSk} onChange={onChangeZkDeposit}/>
+        <h5>amount</h5>
+        <input name='amount' value={zkDeposit.amount} onChange={onChangeZkDeposit}/>
+        <br/>
+        <button>전송</button>
+      </div>
     </div>
   );
 }
